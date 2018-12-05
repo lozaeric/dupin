37058be817ab832eb4f7d1edbcf21d80d832f850
Eric Loza <lozaeric@gmail.com>
2018-12-05 00:28:41 -0300

    remove mock

--- controllers/stores.go
+++ controllers/stores.go
@@ -1,10 +1,7 @@
 package controllers
 
 import (
-	"os"
-
 	"github.com/lozaeric/dupin/domain"
-	"github.com/lozaeric/dupin/mock"
 	"github.com/lozaeric/dupin/mongo"
 	"github.com/lozaeric/dupin/redis"
 )
@@ -15,18 +12,13 @@ var (
 )
 
 func init() {
-	if os.Getenv("ENV") == "production" {
-		var err error
-		userStore, err = redis.NewUserStore()
-		if err != nil {
-			panic(err)
-		}
-		messageStore, err = mongo.NewMessageStore()
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		userStore, _ = mock.NewUserStore()
-		messageStore, _ = mock.NewMessageStore()
+	var err error
+	userStore, err = redis.NewUserStore()
+	if err != nil {
+		panic(err)
+	}
+	messageStore, err = mongo.NewMessageStore()
+	if err != nil {
+		panic(err)
 	}
 }
