e3463758a03a5f2576512667225c47e01f15ef3a
Eric Loza <lozaeric@gmail.com>
2018-12-08 21:25:38 -0300

    toolkit

--- toolkit/utils/utils.go
+++ toolkit/utils/utils.go
@@ -1,7 +1,15 @@
 package utils
 
-import "time"
+import (
+	"time"
+
+	"github.com/rs/xid"
+)
 
 func Now() string {
 	return time.Now().Format(time.RFC3339)
+}
+
+func GenerateID() string {
+	return xid.New().String()
 }
