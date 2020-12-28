25743154b151416533aa003a8f4295597d2ed496
Eric Loza <lozaeric@gmail.com>
2020-12-28 19:26:55 -0300

    bugfixes

--- messages-api/mongo/message_store.go
+++ messages-api/mongo/message_store.go
@@ -61,12 +61,31 @@ func (s *MessageStore) Update(message *domain.Message) error {
 	return conn.DB(database).C(messagesCollection).Update(bson.M{"id": message.ID}, message)
 }
 
+func (s *MessageStore) createIndexes() {
+	indexes := []mgo.Index{
+		mgo.Index{
+			Key: []string{"receiver_id"},
+		},
+		mgo.Index{
+			Key: []string{"sender_id"},
+		},
+		mgo.Index{
+			Key: []string{"seen"},
+		},
+	}
+	for _, idx := range indexes {
+		s.session.DB(database).C(messagesCollection).EnsureIndex(idx)
+	}
+}
+
 func NewMessageStore() *MessageStore {
 	session, err := mgo.DialWithTimeout(connectionString, 2*time.Second)
 	if err != nil {
 		panic(err)
 	}
-	return &MessageStore{
+	s := &MessageStore{
 		session: session,
 	}
+	s.createIndexes()
+	return s
 }
